5744220e1048eb0cf338618fc285b4398a03bf89
Inoyu <117442042+Xia7an@users.noreply.github.com>
2026-01-02 22:52:46 +0900

    feat: レーン押した時の見た目を追加

--- Assets/OctaNotes/Scripts/Play/View/LaneEffectView.cs
+++ Assets/OctaNotes/Scripts/Play/View/LaneEffectView.cs
@@ -1,7 +1,40 @@
+using System;
+using OctaNotes.Scripts.Play.Interface;
+using R3;
+using UnityEngine;
+using Zenject;
+
 namespace OctaNotes.Scripts.Play.View
 {
-    public class LaneEffectView
+    public class LaneEffectView : MonoBehaviour
     {
+        [Inject] private readonly IPlayInputLayer _playInputLayer;
+        [SerializeField] private int laneIndex = 0;
+
+        private Material material;
+        private void Start()
+        {
+            material = GetComponent<MeshRenderer>().material;
+            _playInputLayer.IsButtonPressing[laneIndex].Subscribe(isPressing =>
+            {
+                if (isPressing)
+                {
+                    ToggleOnEffect();
+                }
+                else
+                {
+                    ToggleOffEffect();
+                }
+            });
+        }
+        private void ToggleOnEffect()
+        {
+            material.SetFloat("_Brighten", 1f);
+        }
+        private void ToggleOffEffect()
+        {
+            material.SetFloat("_Brighten", 0f);
+        }
         
     }
 }

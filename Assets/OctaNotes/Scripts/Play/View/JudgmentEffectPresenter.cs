0f1857b37d79f7e5b5963551c9554e9c1b6f3a6e
Inoyu <117442042+Xia7an@users.noreply.github.com>
2026-01-04 22:11:22 +0900

    feat: 判定に応じた音が鳴るようになった

--- Assets/OctaNotes/Scripts/Play/View/JudgmentEffectPresenter.cs
+++ Assets/OctaNotes/Scripts/Play/View/JudgmentEffectPresenter.cs
@@ -8,12 +8,24 @@ namespace OctaNotes.Scripts.Play.View
     /// <summary>
     /// 判定エフェクトの表示管理
     /// </summary>
+    [RequireComponent(typeof(AudioSource))]
     public class JudgmentEffectPresenter : MonoBehaviour, IEffectPresenter
     {
+        [SerializeField] private AudioClip perfectSound;
+        [SerializeField] private AudioClip goodSound;
+        
+        private AudioSource _audioSource;
+        
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+        
         public void PresentJudgment(JudgmentEvent judgmentEvent)
         {
             ShowJudgmentText(judgmentEvent.Result, judgmentEvent.NotePosition, judgmentEvent.EffectTime);
             ShowParticle(judgmentEvent.NotePosition, judgmentEvent.EffectTime);
+            PlaySound(judgmentEvent.Result);
         }
 
         private void ShowJudgmentText(JudgmentResult result, int position, float time)
@@ -21,6 +33,26 @@ namespace OctaNotes.Scripts.Play.View
             // 判定テキストの表示処理
             // TODO: 実際のテキスト表示実装
             Debug.Log($"[JudgmentEffect] {result} at lane {position}, time: {time}");
+        }
+        
+        private void PlaySound(JudgmentResult result)
+        {
+            AudioClip clip = null;
+            switch (result)
+            {
+                case JudgmentResult.Perfect:
+                    clip = perfectSound;
+                    break;
+                case JudgmentResult.Good:
+                    clip = goodSound;
+                    break;
+                // 他の判定結果に応じたサウンドも追加可能
+            }
+
+            if (clip != null)
+            {
+                _audioSource.PlayOneShot(clip);
+            }
         }
 
         private void ShowParticle(int position, float time)
